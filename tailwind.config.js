ad0215c00f547f44015cb2702f31ac53ed091ea7
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2025-01-13 12:16:37 -0500

    feat: tailwind plugin setup, primary and secondary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import undefinedPlugins from './lib/plugins';
+
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}', './lib/**/*.{js,jsx}'],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [undefinedPlugins]
 };
