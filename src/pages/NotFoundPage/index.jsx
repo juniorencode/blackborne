4428ef75e156fe4b6af01944fdcf961f026238a0
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2025-01-13 15:20:40 -0500

    feat: not found layout

---
+++ src/pages/NotFoundPage/index.jsx
@@ -0,0 +1,9 @@
+import { BaseLayout, NotFoundLayout } from '../../../lib/layouts';
+
+export const NotFoundPage = () => {
+  return (
+    <BaseLayout className="p-0">
+      <NotFoundLayout />
+    </BaseLayout>
+  );
+};
