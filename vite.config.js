49d3e8da6549e96c35240d2bbdd6f4edd81a6504
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2025-01-13 10:46:45 -0500

    feat: vite setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { extname, relative, resolve } from 'path';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
+import react from '@vitejs/plugin-react';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+
+import { visualizer } from 'rollup-plugin-visualizer';
+
+// Define __dirname
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    libInjectCss(),
+    visualizer({
+      filename: './stats.html'
+    })
+  ],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'lib/main.js'),
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime', 'react-dom', 'react-router-dom'],
+      input: Object.fromEntries(
+        glob
+          .sync('lib/**/*.{js,jsx}')
+          .map(file => [
+            relative('lib', file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url))
+          ])
+      ),
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js'
+      }
+    }
+  }
+});
