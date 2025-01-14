09affda1fcdb2e7bb2fcb648c00c410401f40034
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2025-01-14 16:11:44 -0500

    feat: input select component

--- src/pages/FormPage/index.jsx
+++ src/pages/FormPage/index.jsx
@@ -1,11 +1,13 @@
 import { DataFormLayout } from '../../../lib/layouts';
 import { useForm } from '../../../lib/hooks';
-import { InputText } from '../../../lib/main';
+import { InputSelect, InputText } from '../../../lib/main';
 
 export const FormPage = () => {
   const Form = useForm({
     text: 'galaxy',
-    textMultiple: ['apple', 'banana', 'cherry']
+    textMultiple: ['apple', 'banana', 'cherry'],
+    select: '2',
+    selectMultiple: ['3', '4', '5']
   });
 
   const breadcrumb = [
@@ -61,6 +63,43 @@ export const FormPage = () => {
           register={Form.register}
           funcDelete={id => console.log('delete item: ' + id)}
           minLength={2}
+        />
+        <InputSelect
+          className="col-span-12"
+          name="select"
+          label="Select"
+          options={[
+            { value: '1', label: 'Lion' },
+            { value: '2', label: 'Wolf' },
+            { value: '3', label: 'Sunset' },
+            { value: '4', label: 'Moonlight' },
+            { value: '5', label: 'Galaxy' },
+            { value: '6', label: 'Ocean' },
+            { value: '7', label: 'Mountain Peak' },
+            { value: '8', label: 'Waterfall' },
+            { value: '9', label: 'Visionary' }
+          ]}
+          funcDelete={id => console.log('delete item: ' + id)}
+          register={Form.register}
+        />
+        <InputSelect
+          className="col-span-12"
+          name="selectMultiple"
+          label="Select"
+          options={[
+            { value: '1', label: 'Lion' },
+            { value: '2', label: 'Wolf' },
+            { value: '3', label: 'Sunset' },
+            { value: '4', label: 'Moonlight' },
+            { value: '5', label: 'Galaxy' },
+            { value: '6', label: 'Ocean' },
+            { value: '7', label: 'Mountain Peak' },
+            { value: '8', label: 'Waterfall' },
+            { value: '9', label: 'Visionary' }
+          ]}
+          funcDelete={id => console.log('delete item: ' + id)}
+          register={Form.register}
+          multiple
         />
       </form>
     </DataFormLayout>
