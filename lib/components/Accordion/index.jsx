e1d082b6745fe0c6d224870a6b93d05cd7e1e3d9
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2025-02-16 12:51:10 -0500

    refactor: accept data in accordion

--- lib/components/Accordion/index.jsx
+++ lib/components/Accordion/index.jsx
@@ -1,17 +1,33 @@
 import PropTypes from 'prop-types';
 import { useState, Children, cloneElement } from 'react';
+import { AccordionItem } from './AccordionItem';
 
 export const Accordion = props => {
   const {
     children,
     className,
+    data,
     multiple = false,
     openFirst,
     openLast,
     openDefault
   } = props;
+
+  const items = data
+    ? data.map((item, index) => (
+        <AccordionItem
+          key={index}
+          title={item.title}
+          subtitle={item.subtitle}
+          disabled={item.disabled}
+        >
+          {item.content}
+        </AccordionItem>
+      ))
+    : children;
+
   const [activeIndices, setActiveIndices] = useState(
-    openDefault && openDefault > 0 && openDefault <= Children.count(children)
+    openDefault && openDefault > 0 && openDefault <= Children.count(items)
       ? [openDefault - 1]
       : openLast
       ? [Children.count(children) - 1]
@@ -34,7 +50,7 @@ export const Accordion = props => {
 
   return (
     <div className={className}>
-      {Children.map(children, (child, index) =>
+      {Children.map(items, (child, index) =>
         cloneElement(child, {
           isActive: activeIndices.includes(index),
           onToggle: () => toggleItem(index)
@@ -44,8 +60,16 @@ export const Accordion = props => {
   );
 };
 Accordion.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node,
   className: PropTypes.string,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      subtitle: PropTypes.string,
+      content: PropTypes.node,
+      disabled: PropTypes.bool
+    })
+  ),
   multiple: PropTypes.bool,
   openFirst: PropTypes.number,
   openLast: PropTypes.number,
